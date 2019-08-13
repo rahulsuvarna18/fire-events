43b1c5ed664fed20f858a36167cc0b09ebc42de9
jack culpan <jackculpan@me.com>
2019-08-13 17:11:29 +0800

    added query input

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,8 +2,8 @@ class EventsController < ApplicationController
   require 'httparty'
   require 'json'
 
-  def getstubhubevents
-    url = 'https://api.stubhub.com/sellers/search/events/v3?city=london&rows=100'
+  def getstubhubevents(location, start_date, end_date)
+    url = "https://api.stubhub.com/sellers/search/events/v3?date=#{start_date}TO#{end_date}&city=#{location}&rows=100"
     headers = {
       Accept: "application/json",
       Authorization: "Bearer NmWpqVwKS2OfJb4PDZY1N0jvI369"
--- test.rb
+++ test.rb
@@ -1,7 +1,7 @@
   require 'httparty'
   require 'json'
 
-    url = 'https://api.stubhub.com/sellers/search/events/v3?city=london&rows=100'
+    url = 'https://api.stubhub.com/sellers/search/events/v3?city=&rows=100'
     headers = {
       Accept: "application/json",
       Authorization: "Bearer NmWpqVwKS2OfJb4PDZY1N0jvI369"
