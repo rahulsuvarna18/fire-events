07573b494a7a517d044b7f796ccbedc5b42f30ef
Rahul Suvarna <rahulsuvarna18@gmail.com>
2019-08-13 12:12:35 +0800

    Added validations and relations to user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :favourites
+  has_many :events, through: :favourites
+  validates :first_name, :last_name, :email_address, :password, :avatar, presence: true
+  validates :email_address, uniqueness: true
+  mount_uploader :avatar, PhotoUploader
 end
+
