8fda113e859ce5c41f4387ae1b8061798c1437e7
jack culpan <jackculpan@me.com>
2019-08-13 12:23:35 +0800

    added validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ApplicationRecord
+  has_many :photos
+  validates :start_time, :start_date, :end_time, :end_date, presense: true
+  validates :price, :location, :lng, :lat, presence: true
 end
