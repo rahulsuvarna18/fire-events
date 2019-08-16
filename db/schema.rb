ad979dd5a5aa5b878327ddbf3108742d076f1b77
jack culpan <jackculpan@me.com>
2019-08-16 14:20:49 +0800

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_15_032344) do
+ActiveRecord::Schema.define(version: 2019_08_16_035420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_08_15_032344) do
     t.float "latitude"
     t.float "longitude"
     t.string "category"
+    t.text "description"
   end
 
   create_table "favourites", force: :cascade do |t|
@@ -53,6 +54,11 @@ ActiveRecord::Schema.define(version: 2019_08_15_032344) do
     t.string "first_name"
     t.string "last_name"
     t.string "avatar"
+    t.string "provider"
+    t.string "uid"
+    t.string "facebook_picture_url"
+    t.string "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
