f4d33ec7a886316a1793b33f516522f2b6222017
jack culpan <jackculpan@me.com>
2019-08-19 11:16:20 +0800

    style changes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,9 +7,9 @@ class EventsController < ApplicationController
   def parse_eventbrite(location, categories = nil, start_date = nil, end_date = nil)
     @events = []
     if start_date.present? && end_date.present?
-      url = "https://www.eventbriteapi.com/v3/events/search?location.address=#{location}&categories=#{categories}&start_date.range_start=#{start_date+"T00:00:00"}&start_date.range_end=#{end_date+"T00:00:00"}&location.within=5km&expand=venue&token=JVGRXXNSW3CLWBYKG7RQ"
+      url = "https://www.eventbriteapi.com/v3/events/search?location.address=#{location}&location.within=50km&categories=#{categories}&start_date.range_start=#{start_date+"T00:00:00"}&start_date.range_end=#{end_date+"T00:00:00"}&expand=venue&token=JVGRXXNSW3CLWBYKG7RQ"
     else
-      url = "https://www.eventbriteapi.com/v3/events/search?location.address=#{location}&categories=#{categories}&location.within=5km&expand=venue&token=JVGRXXNSW3CLWBYKG7RQ"
+      url = "https://www.eventbriteapi.com/v3/events/search?location.address=#{location}&location.within=50km&categories=#{categories}&expand=venue&token=JVGRXXNSW3CLWBYKG7RQ"
     end
     html_file = open(url).read
     html_doc = JSON.parse(html_file)
@@ -29,6 +29,7 @@ class EventsController < ApplicationController
     end
     response = HTTParty.get(url)
     x = JSON.parse(response)
+    @category_count = x["total_items"].to_i
      x["events"]["event"].each do |event|
       @events << Event.new(name: event["title"], description: event["description"], latitude: event["latitude"], longitude: event["longitude"], start_date: event["start_time"], end_date: event["stop_time"], url: event["url"])
     end
@@ -57,10 +58,6 @@ class EventsController < ApplicationController
       @events = parse_eventbrite(params[:location], params[:categories], params[:start_date], params[:end_date]) + geteventfulevents(params[:location], params[:categories], params[:start_date], params[:end_date])
     elsif params[:location].present? && params[:categories].present?
       @events = parse_eventbrite(params[:location], params[:categories]) + geteventfulevents(params[:location], params[:categories])
-    # elsif params[:start_date].present?
-    #   @events = parse_eventbrite(params[:location], params[:categories], params[:start_date])
-    # elsif params[:end_date].present?
-    #   @events = parse_eventbrite(params[:location], params[:categories], params[:end_date])
     elsif params[:location].present?
       @events = parse_eventbrite(params[:location]) + geteventfulevents(params[:location])
     else
@@ -68,18 +65,6 @@ class EventsController < ApplicationController
     end
     return @events
   end
-
-  # def remove_duplicates
-  #   @events = parse_using_params
-  #   @events.each do |event1|
-  #     @events.each do |event2|
-  #       if event1[:name] == event2[:name] && event1[:start_date] == event2[:start_date]
-  #         event.destroy
-  #       end
-  #     end
-  #   end
-  #   return @events
-  # end
 
   def find_eventbrite_category_name(category_id)
     @categories = [
---
+++ app/javascript/components/google-login.js
@@ -0,0 +1,24 @@
+// function onSignIn(googleUser) {
+//   var profile = googleUser.getBasicProfile();
+//   console.log('Given Name: ' + profile.getGivenName());
+//   console.log('Family Name: ' + profile.getFamilyName());
+//   console.log('Image URL: ' + profile.getImageUrl());
+//   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+// }
+
+function googleAuthenticate {
+  gapi.load('auth2', function() {
+    auth2 = gapi.auth2.init({
+      client_id: '529865086294-4tkbq67f0eq1504dojum974gimd1g0fa.apps.googleusercontent.com',
+      fetch_basic_profile: false,
+      scope: 'profile'
+    });
+
+    // Sign the user in, and then retrieve their ID.
+    auth2.signIn().then(function() {
+      console.log(auth2.currentUser.get().getId());
+    });
+  });
+}
+
+export { googleAuthenticate }
