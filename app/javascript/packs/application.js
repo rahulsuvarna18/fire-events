4840cbcbdae54ea1bb0b6b487a2a1ce1b02f380e
Rahul Suvarna <rahulsuvarna18@gmail.com>
2019-08-19 12:19:40 +0800

    map markers are working but not on zoomout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,8 @@
 import "bootstrap";
 import { initCityAutoComplete } from '../plugins/init_autocomplete';
 import { initHeatMap } from '../plugins/google_heatmaps';
-import { clusterMarkers } from '../plugins/google_heatmaps';
 
 initHeatMap();
-clusterMarkers();
 initCityAutoComplete();
 
 
