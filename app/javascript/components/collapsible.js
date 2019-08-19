30e2c63f705865227ef64f3c307bf9d2925e927d
jack culpan <jackculpan@me.com>
2019-08-19 18:02:50 +0800

    merge

--- app/javascript/components/collapsible.js
+++ app/javascript/components/collapsible.js
@@ -1,9 +1,9 @@
 function collapsible = () => {
   $("#minimise").on("click", function(){
-    $("#cards").removeClass("col-3");
-    $("#cards").addClass("col-3");
-    $(this).removeClass("col-3");
-    $(this).addClass("col-3");
+    $("#cards").removeClass(".col-3");
+    $("#cards").addClass(".col-3");
+    $(this).removeClass(".col-3");
+    $(this).addClass(".col-3");
 });
 }
 
