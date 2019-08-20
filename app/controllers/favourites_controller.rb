c1a5b0e3fd07071cf5725d938c53607dc7329300
Nicolas Toulemonde <nicolas.toulemonde@hotmail.fr>
2019-08-20 14:46:45 +0800

    Dashboard front end controller

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -2,12 +2,13 @@ class FavouritesController < ApplicationController
   def create
     @favourite = Favourite.new
     @user = User.find(params[:current_user])
-    @favoutite.save
+    @favourite.save
   end
 
   def destroy
     @favourite = Favourite.find(params[:id])
     @favourite.destroy
+    @origin = request.referer
     respond_to do |format|
       format.html { redirect_to users_dashboard_path, notice: 'Your favourite was deleted' }
       format.js
