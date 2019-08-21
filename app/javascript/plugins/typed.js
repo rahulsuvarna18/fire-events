dc1dc203c30f829dfdc7517fa978dcc5e02dbd7c
Rahul Suvarna <rahulsuvarna18@gmail.com>
2019-08-21 13:57:15 +0800

    Homepage and loader complete

--- app/javascript/plugins/typed.js
+++ app/javascript/plugins/typed.js
@@ -1,12 +1,12 @@
-import Typed from 'loader.js';
+import Typed from 'typed.js';
 
 const typed = () => {
  // const typedTitle = new Typed(‘.marketing-title’, {
  //   strings: [“Never let Mister Bear alone!“],
  //   typeSpeed: 100
  // });
- const typedSubtitle = new Typed(‘.marketing-subtitle’, {
-   strings: [“”, “”],
+ const typedSubtitle = new Typed('#typed', {
+   strings: ["Putting your location in", "Setting up the heatmap", "Compiling the results", "Found the best events for you"],
    typeSpeed: 50,
    showCursor: false,
    loop: true
