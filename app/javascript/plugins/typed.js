ca7e9cd769650125d0a0acb2f42b99f4352dbef8
Rahul Suvarna <rahulsuvarna18@gmail.com>
2019-08-21 15:47:19 +0800

    test

--- app/javascript/plugins/typed.js
+++ app/javascript/plugins/typed.js
@@ -1,13 +1,15 @@
 import Typed from 'typed.js';
 
 const typed = () => {
-
+ // const typedTitle = new Typed(‘.marketing-title’, {
+ //   strings: [“Never let Mister Bear alone!“],
+ //   typeSpeed: 100
+ // });
  const typedSubtitle = new Typed('#typed', {
-   strings: ["Putting your location in", "Setting up the heatmap", "Compiling the results", "Found the best events for you"],
+   strings: ["Finding the best events for you", "Setting up the heatmap", "Compiling the results", "Gathering location information"]
    typeSpeed: 50,
    showCursor: false,
    loop: true
  });
 }
-
 export { typed };
