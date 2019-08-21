ec021e999b5e58113f04bdff9756eaafa7f4f219
Nicolas Toulemonde <nicolas.toulemonde@hotmail.fr>
2019-08-21 16:47:28 +0800

    map dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,21 @@ class UsersController < ApplicationController
 
   def dashboard
     @user = current_user
+    favourites = @user.favourites
+    @events = []
+    favourites.each do |fav|
+      @events << fav.event
+    end
+
+
+
+    @markers = @events.map do |event|
+      {
+        lat: event.latitude,
+        lng: event.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { event: event })
+      }
+    end
+    @event = Event.new
   end
 end
